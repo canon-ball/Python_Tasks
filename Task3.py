107e3a6c28ff57b0e17de6db68e80a791d441963
canon-ball <canon_ball@mail.ru>
2019-12-26 16:03:41 +0300

    main structure changed to list

--- Task3.py
+++ Task3.py
@@ -1,12 +1,12 @@
 # Task3
 # Example: ("QWE",1.1,234),2,(None,7),0,2,(7,7,7),2,(12,),(),3,(5,6),3,100500
 
-main_t = ()
+main_sequence = []
 for elem in eval(input()):
     if type(elem) is tuple:
-        main_t += elem
+        main_sequence += elem
     else:
-        if elem > len(main_t):
+        if elem > len(main_sequence):
             break
-        print(main_t[:elem])
-        main_t = main_t[elem:]
+        print(main_sequence[:elem])
+        main_t = main_sequence[elem:]
